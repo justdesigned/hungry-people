e5b73076aa217cd96264315f46d5d9db10f9eb21
Vasyl <vasiadrevkO@gmail.com>
2020-12-15 21:20:22 +0200

    added slider to header and slider section

--- js/script.js
+++ js/script.js
@@ -1,48 +1,66 @@
-ScrollReveal().reveal('.animation--right', {
-    distance: "50%",
-    origin: "right",
-    rotate: {
-        x: 0,
-        y: 0,
-        z: 0
-    },
-    duration: 1000, 
-    delay: 300
-});
-ScrollReveal().reveal('.animation--left', {
-    distance: "50%",
-    origin: "left",
-    rotate: {
-        x: 0,
-        y: 0,
-        z: 0
-    },
-    duration: 1000, 
-    
-});
-ScrollReveal().reveal('.content-image__square--pos-right', {
-    distance: "50%",
-    origin: "right",
-    rotate: {
-        x: 0,
-        y: 0,
-        z: 90
-    },
-    duration: 1000, 
-    delay: 600
-});
-ScrollReveal().reveal('.content-image__square--pos-left', {
-    distance: "50%",
-    origin: "left",
-    rotate: {
-        x: 0,
-        y: 0,
-        z: -90
-    },
-    duration: 1000, 
-    delay: 600
-});
-ScrollReveal().reveal('.h-content__title', {
-    distance: "20px",
-    origin: "top",
+document.addEventListener('DOMContentLoaded', () => {
+    ScrollReveal().reveal('.animation--right', {
+        distance: "50%",
+        origin: "right",
+        rotate: {
+            x: 0,
+            y: 0,
+            z: 0
+        },
+        duration: 1000,
+        delay: 300
+    });
+    ScrollReveal().reveal('.animation--left', {
+        distance: "50%",
+        origin: "left",
+        rotate: {
+            x: 0,
+            y: 0,
+            z: 0
+        },
+        duration: 1000,
+
+    });
+    ScrollReveal().reveal('.content-image__square--pos-right', {
+        distance: "50%",
+        origin: "right",
+        rotate: {
+            x: 0,
+            y: 0,
+            z: 90
+        },
+        duration: 1000,
+        delay: 600
+    });
+    ScrollReveal().reveal('.content-image__square--pos-left', {
+        distance: "50%",
+        origin: "left",
+        rotate: {
+            x: 0,
+            y: 0,
+            z: -90
+        },
+        duration: 1000,
+        delay: 600
+    });
+    ScrollReveal().reveal('.h-content__title', {
+        distance: "20px",
+        origin: "top",
+    });
+
+    //slick slider
+
+    // document.querySelector('.header__bg').slick();
+
+    $('.h-slider').slick({
+        autoplay: true,
+        arrows: false
+    });
+
+    $('.slider').slick({
+        autoplay: true,
+        arrows: false,
+        slidesToShow: 4
+    });
+
 });
